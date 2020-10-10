968a6f417a25dfd1e12385e0bc35a2cd0fad3e7f
Vatsalya Buddha <58065093+vatsalyabuddha@users.noreply.github.com>
2020-10-10 20:59:34 +0530

    Added components(Form, Todo List)

---
+++ src/components/Todo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Todo({text,todo,todos,setTodos}) {
+
+    const deleteHandler = () => {
+        setTodos(todos.filter(el => el.id !== todo.id ))
+    };
+    const completehandler = () => {
+        setTodos(todos.map(item => {
+            if(item.id===todo.id)
+            {
+                return {
+                    ...item, completed : !item.completed
+                };
+            }
+            return item;
+        } ))
+    }
+
+    return (
+        <div className="todo">
+            <li className= {`todo-item ${todo.completed? "completed" : ""  }`}> {text}  </li>
+            <button onClick={completehandler} className="complete-btn"> 
+                <i className="fas fa-check"></i> 
+            </button>
+            <button onClick={deleteHandler} className="trash-btn"> 
+                <i className="fas fa-trash"></i>
+            </button>
+        </div>
+    );
+};
+
+export default Todo;
