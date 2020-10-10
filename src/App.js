9e65ad8643cfe3b97c6eb460312b03aefdf32f70
Vatsalya Buddha <58065093+vatsalyabuddha@users.noreply.github.com>
2020-10-10 20:56:09 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+
+  const [inputText,setInputText] = useState("");
+  const [todos,setTodos] = useState([]);
+  const [status,setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect (() => {
+    const filterHandler= () => {
+      switch(status){
+        case "completed":
+          setFilteredTodos(todos.filter(toodo => todos.completed === true));
+          break;
+        case "uncompleted":
+          setFilteredTodos(todos.filter(toodo => todos.completed === false));
+          break;
+        default :
+          setFilteredTodos(todos);
+          break;
+      }
+    }
+
+    filterHandler();
+  }, [todos, status]);
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>Vatsalya's Todo List</h1>
+    </header>
+    <Form 
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText} 
+        setStatus={setStatus}
+  
+      />
+    <TodoList filteredTodos={filteredTodos} setTodos={setTodos} todos={todos}  />
     </div>
   );
 }
